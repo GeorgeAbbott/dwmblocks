86855fb999d52c200bace400f8995eb6cca6f5af
self <self@sateoki.xyz>
2022-03-23 22:51:52 +0000

    Added icons

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	   /*Icon*/                 /*Command*/                                                   /*Update Interval*/	/*Update Signal*/
-    {  "",                      "mpc current -f %title%",                                        1,                  0                  },
-	{  "Mem:",                  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",       30,                 0                  },
+    {  " ",                    "mpc current -f %title%",                                        1,                  0                  },
+	{  " ",                    "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",       20,                 0                  },
 
-	{  "",                       "date '+%Y-%m-%d %H:%M:%S'",                                    1,                  0                  },
+	{  " ",                    "date '+%Y-%m-%d %H:%M:%S'",                                     1,                  0                  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
