c1c431d7018e65f88335ba5ee2112a706de88f52
otg <otg@sateoki.xyz>
2022-03-30 00:01:21 +0100

    Added battery monitor

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	   /*Icon*/                 /*Command*/                                                   /*Update Interval*/	/*Update Signal*/
+    {  "B ",                    "for bat in /sys/class/power_supply/BAT*; do echo \"$(cat $bat/capacity)%\" | tr '\\n' ' '; done",
+                                                                                                 2,                  0                  },
     {  " ",                    "mpc current -f %title%",                                        1,                  0                  },
 	{  " ",                    "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",       20,                 0                  },
 
@@ -10,3 +12,7 @@ static const Block blocks[] = {
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+
+
+
+// $(for bat in /sys/class/power_supply/BAT* ; echo "$(cat $bat/capacity)%" | tr '\n' ' '
